608b689656532655ea2bbb069385f5ad5b7ff49b
Vandolf Estrellado <vestrel00@gmail.com>
2012-12-19 20:03:30 -0500

    Part 1 Project set up, main class, Camera, Background, interfaces, Settings, Map
    Signed-off-by: Vandolf Estrellado <vestrel00@gmail.com>

---
+++ KungFuNekko/src/com/vestrel00/nekko/interf/Drawable.java
@@ -0,0 +1,9 @@
+package com.vestrel00.nekko.interf;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface Drawable {
+
+	public void draw(SpriteBatch batch);
+
+}
---
+++ KungFuNekko/src/com/vestrel00/nekko/maps/Map.java
@@ -0,0 +1,28 @@
+package com.vestrel00.nekko.maps;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.vestrel00.nekko.interf.Drawable;
+import com.vestrel00.nekko.interf.Updatable;
+
+public class Map implements Updatable, Drawable {
+
+	public float width, height;
+	
+	public Map(){
+		width = 1000.0f;
+		height = 232.0f;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
